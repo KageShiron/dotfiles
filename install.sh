86d62a879065932b0ea6de3130ee6b34df007357
KageShiron <kageshiron@gmail.com>
2016-07-11 02:38:10 +0900

    add auto make ~/.vim and auto install cmigemo

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
+askYesOrNo() {
+    while true ; do
+        read -p "$1 (y/n)?" answer
+        case $answer in
+            [yY] | [yY]es | YES )
+                return 0;;
+            [nN] | [nN]o | NO )
+                return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+if [ -ne "~/.vim" ]; then
+    mkdir ~/.vim
+fi
+
 ln -si ~/dotfiles/.vimrc ~/.vimrc
 ln -si ~/dotfiles/.gvimrc ~/.gvimrc
 ln -si ~/dotfiles/dein.toml ~/.vim/dein.toml
+
+type apt-get
+apt=$?
+type cmigemo
+
+if [ $apt -e 0 && $? -ne 0 ]; then
+    askYesOrNo "Do you want to run \"sudo apt-get install cmigemo\"?"
+    if [ $? -e 0 ]; then
+        sudo apt-get install cmigemo
+    fi
+fi
+
