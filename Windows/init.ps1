0c4d3f85465d64d913d57dcf529c8feef4230f9f
KageShiron <kageshiron@gmail.com>
2017-05-02 22:09:37 +0900

    Add install some packages

--- Windows/init.ps1
+++ Windows/init.ps1
@@ -7,6 +7,11 @@ if( -not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 cinst packages.config
 
+npm install -g typescript typings webpack react react-dom babel-loader babel-core vue-cli
+gem install jekyll
+
+
+
 if( -not ( Test-Path ~/.vimrc )){ cmd /c "mklink `"%userprofile%/.vimrc`" `"../.vimrc`"" }
 if( -not ( Test-Path ~/.gvimrc )){ cmd /c "mklink `"%userprofile%/.vimrc`" `"../.gvimrc`"" }
 if( -not ( Test-Path ~/.ssh )){ cmd /c "mklink /d `"%userprofile%/.ssh`" `"c:/gd/sync/ssh`"" }([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
