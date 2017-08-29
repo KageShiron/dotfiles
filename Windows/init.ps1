b7e0bf43ab818d7127b861b2cc8551be14362155
KageShiron <kageshiron@gmail.com>
2017-08-29 21:28:47 +0900

    環境変数がらみの修正 タイポ修正

--- Windows/init.ps1
+++ Windows/init.ps1
@@ -23,33 +23,42 @@ if ( -not (Test-Path ~/.ssl/cacert.pem) ) {
 }
 
 # init dir
-if ( -not ( Test-Path ~/.vimrc )) { cmd /c (("mklink `"%userprofile%/.vimrc`" `"") + (Resolve-Path ../.vimrc).Path + "`"") }
-if ( -not ( Test-Path ~/.gvimrc )) { cmd /c (("mklink `"%userprofile%/.gvimrc`" `"") + (Resolve-Path ../.gvimrc).Path + "`"") }
+if ( -not ( Test-Path ~/.vimrc )) { 
+    cmd /c (("mklink `"%userprofile%/.vimrc`" `"") + (Resolve-Path ../.vimrc).Path + "`"") 
+}
+if ( -not ( Test-Path ~/.gvimrc )) {
+    cmd /c (("mklink `"%userprofile%/.gvimrc`" `"") + (Resolve-Path ../.gvimrc).Path + "`"") 
+}
 if ( -not ( Test-Path ~/.ssh )) { cmd /c "mklink /d `"%userprofile%/.ssh`" `"c:/gd/sync/ssh`"" }
 if ( -not ( Test-Path ~/.vim )) { mkdir ~/.vim }
 if ( -not ( Test-Path "c:/app" )) { mkdir "c:/app" }
-if ( -not ( Test-Path ~/.vim/dein.toml ) ) {  cmd /c (("mklink `"%userprofile%/.vim/dein.toml`" `"") + (Resolve-Path ../dein.toml).Path + "`"") }
+if ( -not ( Test-Path ~/.vim/dein.toml ) ) {
+    cmd /c (("mklink `"%userprofile%/.vim/dein.toml`" `"") + (Resolve-Path ../dein.toml).Path + "`"") 
+}
+if ( -not ( Test-Path "C:/tools/cmder/vendor/conemu-maximus5") ) {  
+    cmd /c ("mklink `"C:/tools/cmder/vendor/conemu-maximus5`" `"C:/src/dotfiles/Windows/ConEmu.xml`"")
+}
 
 $path = [Environment]::GetEnvironmentVariable('PATH', 'Machine')
 $path2 = [Environment]::GetEnvironmentVariable('PATH', 'User')
-$env:path = $path + $path2;
+$env:path = $path + ";" + $path2;
 
 $gopath = go env GOPATH
 if ( -not $path.contains($gopath) ) {
-    $path += ';' + $gopath;
+    $path += ';' + $gopath + "\\bin";
 }
 [Environment]::SetEnvironmentVariable('PATH', $path, 'Machine')
-$env:path = $path + $path2
+$env:path = $path + ";" + $path2
 
 yarn global add -g typescript typings webpack react react-dom babel-loader babel-core vue-cli
 gem install rubygems-update --source http://rubygems.org/
 update_rubygems
-gem install --conservation bundle jekyll
+gem install --conservative bundle jekyll
 
 go get github.com/mattn/sudo
 go get github.com/mattn/goemon/cmd/goemon
 go get github.com/koron/netupvim
-cd c:\app
+cd c:/app
 netupvim
 
 tlmgr update --self —all
