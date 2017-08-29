2c4caee1c13442bc583ec0b3c8ee6a7f0a160959
KageShiron <kageshiron@gmail.com>
2017-08-29 20:53:46 +0900

    環境変数がらみを調整

--- Windows/init.ps1
+++ Windows/init.ps1
@@ -22,15 +22,7 @@ if ( -not (Test-Path ~/.ssl/cacert.pem) ) {
     [Environment]::SetEnvironmentVariable('SSL_CERT_FILE', ($env:userprofile + ''), 'User')
 }
 
-yarn global install -g typescript typings webpack react react-dom babel-loader babel-core vue-cli
-gem install rubygems-update --source http://rubygems.org/
-update_rubygems
-gem install bundle jekyll
-
-
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-
-if ( -not (Test-Path c:\app )) { mkdir "c:/app" }
+# init dir
 if ( -not ( Test-Path ~/.vimrc )) { cmd /c (("mklink `"%userprofile%/.vimrc`" `"") + (Resolve-Path ../.vimrc).Path + "`"") }
 if ( -not ( Test-Path ~/.gvimrc )) { cmd /c (("mklink `"%userprofile%/.gvimrc`" `"") + (Resolve-Path ../.gvimrc).Path + "`"") }
 if ( -not ( Test-Path ~/.ssh )) { cmd /c "mklink /d `"%userprofile%/.ssh`" `"c:/gd/sync/ssh`"" }
@@ -38,12 +30,28 @@ if ( -not ( Test-Path ~/.vim )) { mkdir ~/.vim }
 if ( -not ( Test-Path "c:/app" )) { mkdir "c:/app" }
 if ( -not ( Test-Path ~/.vim/dein.toml ) ) {  cmd /c (("mklink `"%userprofile%/.vim/dein.toml`" `"") + (Resolve-Path ../dein.toml).Path + "`"") }
 
+$path = [Environment]::GetEnvironmentVariable('PATH', 'Machine')
+$path2 = [Environment]::GetEnvironmentVariable('PATH', 'User')
+$env:path = $path + $path2;
+
+$gopath = go env GOPATH
+if ( -not $path.contains($gopath) ) {
+    $path += ';' + $gopath;
+}
+[Environment]::SetEnvironmentVariable('PATH', $path, 'Machine')
+$env:path = $path + $path2
+
+yarn global add -g typescript typings webpack react react-dom babel-loader babel-core vue-cli
+gem install rubygems-update --source http://rubygems.org/
+update_rubygems
+gem install --conservation bundle jekyll
+
 go get github.com/mattn/sudo
 go get github.com/mattn/goemon/cmd/goemon
 go get github.com/koron/netupvim
 cd c:\app
 netupvim
 
-
 tlmgr update --self —all
 tlmgr install collection-langjapanese collection-luatex collection-latexextra minted latexmk
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
